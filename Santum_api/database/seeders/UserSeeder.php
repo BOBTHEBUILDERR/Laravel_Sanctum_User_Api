2516b64bf8f9e01797f7b65ac82fce0dd7da067b
Mahesh <laterm57@gmail.com>
2023-05-19 00:08:57 +0530

    timezone , migration seed and controller

---
+++ Santum_api/database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (!DB::table('users')->where('email', 'mickvm3@gmail.com')->exists()) {
+
+            DB::table('users')->insert([
+                'fname' => 'Mahesh ',
+                'lname' => 'Verma ',
+                'email' => 'mickvm3@gmail.com',
+                'password' => Hash::make('password'),
+                'created_at' =>  Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+    }
+}
--- Santum_api/routes/api.php
+++ Santum_api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('test',function(){
+     return 'hello';
+    });
+    Route::any('change_password',[APIController::class,'changePassword']);
+    Route::post('logout',[APIController::class,'logout']);
+    Route::post('tokens_delete',[APIController::class,'logoutAll']);
+    Route::post('profile',[APIController::class,'profileUpdate']);
+    Route::post('profile-pic', [APIController::class,'updateProfilePic']);
+    Route::post('profile-get', [APIController::class,'profile_get']);
+    Route::get('get-links', [APIController::class,'links']);
+    Route::get('get-user', [APIController::class,'get_user']);
+ 
+ });
+ Route::any('signup',[APIController::class,'signup']);
+ Route::any('login',[APIController::class,'login'])->name('login');
+ // Route::any('forget_password',[APIController::class,'submitForgetPasswordForm']);
+ // Route::any('/password/reset', [APIController::class,'resetPassword'])->name('password.reset');
+ Route::any('reset_password', [APIController::class,'apiReset']);
+ Route::any('reset', [APIController::class,'apiSendResetLinkEmail'])->name('password.reset');
+ 
