91924b88c98e38456023ce227b1707f034f0f949
BOBTHEBUILDERR <38834484+BOBTHEBUILDERR@users.noreply.github.com>
2023-05-19 13:19:03 +0530

    Update HomeController.php

--- Santum_api/app/Http/Controllers/API/HomeController.php
+++ Santum_api/app/Http/Controllers/API/HomeController.php
@@ -10,16 +10,7 @@ class HomeController extends Controller
     public function links(){
 
         $links = [
-            ['name'=>'oahu', 'links'=>'https://shor.by/oahu'],
-            ['name'=>'lasvegas', 'links'=>'https://shor.by/lasvegas'],
-            ['name'=>'bigislandhawaii', 'links'=>'https://shor.by/bigislandhawaii'],
-            ['name'=>'newyorknow', 'links'=>'https://shor.by/newyorknow'],
-            ['name'=>'phoenixaz', 'links'=>'https://shor.by/phoenixaz'],
-            ['name'=>'destin','links'=>'https://shor.by/destin'],
-            ['name'=>'mauinow', 'links'=>'https://shor.by/mauinow'],
-            ['name'=>'laketahoenow', 'links'=>'https://shor.by/laketahoenow'],
-            ['name'=>'sedona', 'links'=>'https://shor.by/sedona'],
-            ['name'=>'Kauai', 'links'=>'https://shor.by/Kauai']
+            'name'=> 'links'
         ];
 
         $data = [
